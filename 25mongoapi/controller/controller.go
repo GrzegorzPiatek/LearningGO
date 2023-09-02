90a3a6d3c595d14e21a1bd0008255f8122e7c7da
Grzegorz <grzegorz.piatek@scan-it.pl>
2023-09-02 11:23:44 +0200

    Add controllers methods

--- 25mongoapi/controller/controller.go
+++ 25mongoapi/controller/controller.go
@@ -3,12 +3,15 @@ package controller
 import (
 	"25mongoapi/model"
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"net/http"
 )
 
 const connectionString = "mongodb+srv://<root>:<root>@cluster0.9jw7kca.mongodb.net/?retryWrites=true&w=majority"
@@ -31,6 +34,53 @@ func init() {
 	collection = client.Database(dbName).Collection(colName)
 
 	fmt.Println("Collection is ready.")
+}
+
+func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	allMovies := getAllMovies()
+
+	_ = json.NewEncoder(w).Encode(allMovies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var movie model.Netflix
+
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	insertOneMovie(movie)
+	_ = json.NewEncoder(w).Encode(movie)
+}
+
+func MarkAsWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	params := mux.Vars(r)
+
+	updateOneMovie(params["id"])
+	_ = json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteOneMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+
+	params := mux.Vars(r)
+
+	deleteOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+
+	count := deleteAllMovies()
+	json.NewEncoder(w).Encode(count)
 }
 
 // HELPERS
@@ -63,11 +113,13 @@ func deleteOneMovie(movieId string) {
 	fmt.Println("One delete with count: ", result.DeletedCount)
 }
 
-func deleteAllMovies() {
+func deleteAllMovies() int64 {
 	result, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
 	fatalLog(err)
 
 	fmt.Println("Number of movies deleted: ", result.DeletedCount)
+
+	return result.DeletedCount
 }
 
 func getAllMovies() []primitive.M {
